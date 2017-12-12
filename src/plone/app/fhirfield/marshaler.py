1515fcec1afa312c4a15e63e04360af2253b33d2
Md Nazrul Islam <nazrul@zitelab.dk>
2017-12-12 16:27:42 +0100

    1. plone.rfc822 marshaler field support added

---
+++ src/plone/app/fhirfield/marshaler.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from plone.app.fhirfield.interfaces import IFhirResource
+from plone.rfc822.defaultfields import BaseFieldMarshaler
+from zope.component import adapter
+from zope.interface import Interface
+
+
+@adapter(Interface, IFhirResource)
+class FhirResourceFieldMarshaler(BaseFieldMarshaler):
+    """Field marshaler for plone.app.fhirfield values."""
+
+    ascii = False
+
+    def encode(self, value, charset='utf-8', primary=False):
+        if value is None:
+            return
+        actual_value = value.stringify()
+        if not actual_value:
+            return
+        return actual_value.encode(charset)
+
+    def decode(self, value, message=None, charset='utf-8', contentType=None, primary=False):
+        try:
+            unicode_value = value.decode(charset)
+        except UnicodeEncodeError:
+            unicode_value = value  # was already unicode
+
+        if unicode_value in ('', None):
+            decoded_value = IFhirResource(self.field).from_none(unicode_value)
+        else:
+            decoded_value = IFhirResource(self.field).fromUnicode(unicode_value)
+        return decoded_value
+
+    def getContentType(self):
+        value = self._query()
+        if value is None:
+            return None
+        return 'application/json'
+
+    def getCharset(self, default='utf-8'):
+        value = self._query()
+        if value is None:
+            return None
+        return getattr(value, '_encoding', default)
--- src/plone/app/fhirfield/tests/test_schemaeditor.py
+++ src/plone/app/fhirfield/tests/test_schemaeditor.py
@@ -2,8 +2,8 @@
 from .schema import ITestOrganization
 from plone.app.fhirfield.testing import PLONE_APP_FHIRFIELD_INTEGRATION_TESTING
 from plone.schemaeditor.interfaces import IFieldFactory
+from zope.component import getUtilitiesFor
 from zope.component import queryUtility
-from zope.component import queryUtilityFor
 from zope.schema import getFields
 
 import unittest
@@ -35,5 +35,7 @@ class SchemaeditorIntegrationTest(unittest.TestCase):
         self.assertEqual(fhir_field.model, fhir_field2.model)
         self.assertEqual(fhir_field, fhir_field2)
 
-        field_list = [x for x in queryUtilityFor(IFieldFactory) if x[0] == 'plone.app.fhirfield.field.FhirResource']
+        field_list = [x for x in getUtilitiesFor(IFieldFactory) if x[0] == 'plone.app.fhirfield.field.FhirResource']
         self.assertEqual(1, len(field_list))
+        # http://localhost:8080/site/dexterity-types/hellotype/@@add-field?ajax_load=1513025812202
+        # http://localhost:8080/site/dexterity-types/@@add-type?ajax_load=1513025868528
