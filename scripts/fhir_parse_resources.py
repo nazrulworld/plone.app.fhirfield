4fa5f7358c2dffca29d106c4fcab89aebd2989c8
Md Nazrul Islam <connect2nazrul@gmail.com>
2018-08-03 15:53:08 +0200

    [WIP] fhir resource parser updated as resource list creation feature updated

--- scripts/fhir_parse_resources.py
+++ scripts/fhir_parse_resources.py
@@ -11,12 +11,17 @@ from helpers import cmd
 
 import aiohttp
 import asyncio
+import datetime
 import hashlib
 import io
+import json
 import os
 import pathlib
+import shutil
 import sys
+import tempfile
 import tqdm
+import zipfile
 
 
 __author__ = 'Md Nazrul Islam (email2nazrul@gmail.com)'
@@ -28,7 +33,8 @@ FHIR_DEFINITION = 'definitions.json' + EXT
 FHIR_EXAMPLE = 'examples-json' + EXT
 FHIR_SCHEMAS = 'fhir.schema.json' + EXT
 OUTPUT_DIR = pathlib.Path(__file__).parent.parent / 'src' / 'plone' / 'app' / \
-    'fhirfield' / 'browser' / 'static' / 'FHIR' / 'HL7' / 'search'
+    'fhirfield' / 'browser' / 'static' / 'FHIR' / 'HL7'
+RESOURCES_DIR = OUTPUT_DIR / 'ResourceList'
 CACHE_DIR = pathlib.Path(__file__).parent / '.cache'
 
 
@@ -69,7 +75,7 @@ async def download_archieves(uris, session, offline):
                     'going to dowanload fresh'.
                     format(cached_file))
             else:
-                files.append(cached_file)
+                files.append((uri, cached_file, ))
                 continue
 
         try:
@@ -79,7 +85,7 @@ async def download_archieves(uris, session, offline):
                     sys.stdout.write('Start downloading file from {0}\n'.
                                      format(uri))
                     await write_stream(str(cached_file), response)
-                    files.append(cached_file)
+                    files.append((uri, cached_file, ))
 
         except (ServerDisconnectedError,
                 ClientResponseError,
@@ -90,6 +96,52 @@ async def download_archieves(uris, session, offline):
     return files
 
 
+async def make_save_resources_list(archive_file, container):
+    """ """
+    tmp_dir = tempfile.mkdtemp()
+
+    with zipfile.ZipFile(archive_file, 'r') as zip_ref:
+        zip_ref.extractall(tmp_dir)
+
+    for root, dirs, files in os.walk(tmp_dir):
+
+        for filename in files:
+
+            if not filename.endswith('.schema.json'):
+                continue
+            with open(os.path.join(root, filename), 'r') as f:
+                content = json.load(f)
+
+            if content.get('id', None) is None:
+                continue
+            resource_name = content['id'].split('/')[-1]
+
+            assert filename.split('.')[0] == resource_name
+
+            container['resources'][resource_name.lower()] = {
+                'name': resource_name,
+                'url': content['id'] + '.schema.json'
+            }
+
+    container['meta']['lastUpdated'] = datetime.datetime.now().isoformat('T')
+
+    # write to disk
+    global RESOURCES_DIR
+    global VERSION
+
+    if not RESOURCES_DIR.exists():
+        RESOURCES_DIR.mkdir()
+
+    target = RESOURCES_DIR / '{0}.json'.format(VERSION)
+
+    with open(str(target), 'w') as f:
+        json.dump(container, f, indent=4, sort_keys=True)
+
+    sys.stdout.write('==> file {0} has been created\n'.format(target))
+
+    shutil.rmtree(tmp_dir)
+
+
 @cmd
 async def main(destination_dir, output_stream, offline, verbosity_level):
     """ """
@@ -97,10 +149,21 @@ async def main(destination_dir, output_stream, offline, verbosity_level):
     #     response = requests.get(RESOURCE_URL)
     # except requests.RequestException as e:
     #     raise e
+    global BASE_URL
+    global VERSION
+    global FHIR_DEFINITION
+    global FHIR_EXAMPLE
+    global FHIR_SCHEMAS
+
     archived_files = ('/'.join([BASE_URL, VERSION, FHIR_DEFINITION]),
                       '/'.join([BASE_URL, VERSION, FHIR_EXAMPLE]),
                       '/'.join([BASE_URL, VERSION, FHIR_SCHEMAS]))
 
     async with aiohttp.ClientSession() as session:
         results = await download_archieves(archived_files, session, offline)
-        print(results)
+        for url, location in results:
+            if FHIR_SCHEMAS in url:
+                container = dict(
+                    meta=dict(profile=url, versionId=VERSION),
+                    resources=dict())
+                await make_save_resources_list(location, container)
