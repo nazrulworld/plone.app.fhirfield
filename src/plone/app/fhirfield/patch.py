31e164dc8f5e24e2a89a85bb77337c14bfa90d69
Md Nazrul Islam <connect2nazrul@gmail.com>
2018-10-24 16:55:12 +0200

    [WIP] adds missing fields for IResourceRegistry for plone 4 #11

--- src/plone/app/fhirfield/patch.py
+++ src/plone/app/fhirfield/patch.py
@@ -9,7 +9,8 @@ def monkey_patch_fhir_base_model():
     import fhirclient.models.resource
     from plone.app.fhirfield.interfaces import IFhirResourceModel
     # We forcely implement IFhirResourceModel
-    fhirclient.models.resource.Resource = implementer(IFhirResourceModel)(fhirclient.models.resource.Resource)
+    fhirclient.models.resource.Resource = \
+        implementer(IFhirResourceModel)(fhirclient.models.resource.Resource)
 
 
 def fix_resource_registry_import_error():
@@ -18,10 +19,26 @@ def fix_resource_registry_import_error():
     try:
         Products.CMFPlone.interfaces.IResourceRegistry
     except AttributeError:
+        from zope import schema
         from zope.interface import Interface
 
         class IResourceRegistry(Interface):
             """ """
+
+            js = schema.ASCIILine(
+                title=u'Main js file',
+                required=False)
+
+            css = schema.List(
+                title=u'CSS/LESS files',
+                value_type=schema.ASCIILine(title=u'URL'),
+                default=[],
+                required=False)
+
+            deps = schema.ASCIILine(
+                title=u'Dependencies for shim',
+                description=u'Comma separated values of resource for shim',
+                required=False)
         # Make fall back for plone 4
         setattr(
             Products.CMFPlone.interfaces,
