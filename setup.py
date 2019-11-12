eabc7c94c8fd5cca97779195eb7ce45a0c9426b1
Md Nazrul Islam <connect2nazrul@gmail.com>
2019-11-12 23:22:44 +0600

    Back to development: 3.0.0b3

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ test_requires = [
 
 setup(
     name="plone.app.fhirfield",
-    version="3.0.0b2",
+    version="3.0.0b3.dev0",
     description="FHIR field for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/pypi?%3Aaction=list_classifiers
